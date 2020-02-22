08b9f55ef9eb54cd5f8d8693cc4ef58b9da177cf
nealthom <nealthom@gmail.com>
2020-02-22 06:42:15 -0600

    introducing p5.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,25 @@
 import React from "react"
 import Layout from "../components/layout"
+import P5Wrapper from "react-p5-wrapper"
 
 export default () => {
+  const sketch = p => {
+    let t = 0
+
+    p.setup = function() {
+      p.createCanvas(600, 400)
+      p.background(255)
+    }
+
+    p.draw = function() {
+      p.ellipse(100, 180, 16, 16)
+    }
+  }
   return (
     <Layout>
       <div>
         <h1>Welcome</h1>
+        <P5Wrapper sketch={sketch} />
       </div>
     </Layout>
   )
