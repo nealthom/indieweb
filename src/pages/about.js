1d84dbd93c7ae760c3abd7821e9c45138fac91ef
nealthom <nealthom@gmail.com>
2020-02-21 23:57:37 -0600

    update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,12 @@
 import React from "react"
 import { graphql } from "gatsby"
-//import Layout from "../components/layout"
+import Layout from "../components/layout"
 
 export default ({ data }) => (
-  // <Layout>
-  <h1>About {data.site.siteMetadata.title}</h1>
-  // <p>I'm many things in Hot Springs, Arkansas.</p>
-  // </Layout>
+  <Layout>
+    <h1>About {data.site.siteMetadata.title}</h1>
+    <p>I'm many things in Hot Springs, Arkansas.</p>
+  </Layout>
 )
 
 export const query = graphql`
