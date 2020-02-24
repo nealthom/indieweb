524c2574545de7b38b4bb03912e392ce8200a329
nealthom <nealthom@gmail.com>
2020-02-24 04:14:42 -0600

    p5 testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,24 @@
 import React from "react"
 import Layout from "../components/layout"
-import P5Wrapper from "react-p5-wrapper"
+import Sketch from "../components/sketch"
+import sketch from "../components/scripts/phi"
 
 export default () => {
-  const sketch = p => {
-    p.setup = function() {
-      p.createCanvas(600, 400)
-      p.background(255)
-    }
+  // const sketch = p => {
+  //   p.setup = function() {
+  //     p.createCanvas(600, 400)
+  //     p.background(255)
+  //   }
 
-    p.draw = function() {
-      p.ellipse(100, 180, 16, 16)
-    }
-  }
+  //   p.draw = function() {
+  //     p.ellipse(100, 180, 16, 16)
+  //   }
+  // }
   return (
     <Layout>
       <div>
         <h1>Welcome</h1>
-        <P5Wrapper sketch={sketch} />
+        <Sketch sketch={sketch} />
       </div>
     </Layout>
   )
