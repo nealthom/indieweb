c3416238052f342b8e9905a7ebd90c262ee81dea
nealthom <nealthom@gmail.com>
2020-03-02 23:40:51 -0600

    gatsby deploy

--- src/p5/scripts/particles.js
+++ src/p5/scripts/particles.js
@@ -1,4 +1,4 @@
-import Particle from "../classes/Particle"
+//
 
 export default p => {
   const total = 50
@@ -23,5 +23,34 @@ export default p => {
       particle.run()
     }
     particles = particles.filter(particle => !particle.isDead())
+  }
+}
+
+class Particle {
+  constructor(p, l) {
+    this.p = p
+    this.position = l.copy()
+    this.acceleration = p.createVector(0, 0.05)
+    this.velocity = p.createVector(p.random(-1, 1), p.random(-2, 0))
+    this.lifespan = 255
+  }
+  update() {
+    this.velocity.add(this.acceleration)
+    this.position.add(this.velocity)
+    this.lifespan -= 2.0
+  }
+  display() {
+    this.p.stroke(0, this.lifespan)
+    this.p.fill(175, this.lifespan)
+    this.p.ellipse(this.position.x, this.position.y, 8, 8)
+  }
+
+  isDead() {
+    return this.lifespan < 0.0
+  }
+
+  run() {
+    this.update()
+    this.display()
   }
 }
