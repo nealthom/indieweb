ec6910ac84707d82079f3a60cc8aace342d5b2a0
nealthom <nealthom@gmail.com>
2020-03-02 23:48:20 -0600

    gatsby

--- src/p5/scripts/particles.js
+++ src/p5/scripts/particles.js
@@ -52,3 +52,41 @@
 //     this.display()
 //   }
 // }
+
+/* eslint-disable no-multi-assign */
+/* eslint-disable no-param-reassign */
+
+// We'll ride the spiral to the end and may just go where no one's been
+// Spiral out, keep going ...
+
+export default p => {
+  const phi = 1.6180339
+  p.state = []
+  p.dispatch = () => {}
+
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight)
+  }
+
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight)
+    p.noFill()
+    p.smooth()
+  }
+
+  p.draw = () => {
+    p.background("#e9e2d7")
+    p.stroke("pink")
+    p.spiral(p.width / 2, p.height / 2, 0, 300)
+  }
+
+  p.spiral = (x, y, origin, lines) => {
+    for (let i = 0; i < lines; i += 1) {
+      p.translate(x, y)
+      p.rotate(p.radians(p.noise((p.frameCount - i) * 0.001 * phi) * 10 + 100))
+      p.line(origin, 0, 0, 0)
+      p.translate(-x, -y)
+      x += origin += phi
+    }
+  }
+}
