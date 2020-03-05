9fa21c335c6fc50bd1e676da29d54ea3aadf671e
nealthom <nealthom@gmail.com>
2020-03-04 21:48:27 -0600

    update landing page

--- src/components/scripts/phi.js
+++ src/components/scripts/phi.js
@@ -8,6 +8,9 @@ export default p => {
   const phi = 1.6180339
   p.state = []
   p.dispatch = () => {}
+  let r = 255,
+    g = 10,
+    b = 0
 
   p.windowResized = () => {
     p.resizeCanvas(p.windowWidth, p.windowHeight)
@@ -20,8 +23,20 @@ export default p => {
   }
 
   p.draw = () => {
+    r++
+    if (r > 255) {
+      g++
+    }
+    if (g > 255) {
+      b++
+    }
+    if (b > 255) {
+      r = 255
+      g = 10
+      b = 0
+    }
     p.background("#e9e2d7")
-    p.stroke("pink")
+    p.stroke(r, g, b)
     p.spiral(p.width / 2, p.height / 2, 0, 300)
   }
 
